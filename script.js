87b1ada747bca3a848eaaf12d30ad15daf086236
Bubba <bubba.dave.7@gmail.com>
2021-11-14 19:22:26 -0700

    added user input variables and character values

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// input variables: 
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+// special character values 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// numeric values
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// alphabetical values
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// uppercase conversion value
+space = [];
+// concatenation upon condition
+var choices;
+// alows letters to convert to uppercase 
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
+// variable for uppercase conversion
+alpha2 = alpha.map(toUpper);
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
+
 
 // Write password to the #password input
 function writePassword() {
